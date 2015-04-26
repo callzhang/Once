5f57517221604e984ce765ab6d2791822879b67c
Lei Zhang <leizhang0121@gmail.com>
2015-04-26 19:51:20 -0400

    added notification view

---
+++ Remember/controllers/CRNotificationsViewController.h
@@ -0,0 +1,13 @@
+//
+//  CRNotificationsViewController.h
+//  Remember
+//
+//  Created by Lee on 4/26/15.
+//  Copyright (c) 2015 Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CRNotificationsViewController : UITableViewController
+
+@end
